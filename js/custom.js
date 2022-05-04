1186a204e4f669736279d75d226afcb506f7ae43
John Paulo <jpgabad52@gmail.com>
2022-05-04 12:37:15 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+
+// WOW JS
+new WOW().init();
+var scroll = new SmoothScroll('a[href*="#"]', {
+    ignore: '[data-scroll-ignore]'
+});
+
+$(window).scroll(function () {
+    if ($(document).scrollTop() > 100) {
+        $('.z-navbar1').removeClass('onscroll-show');
+        $('.z-navbar1').addClass('onscroll-hide');
+        $('.z-navbar2').removeClass('onscroll-hide');
+        $('.z-navbar2').addClass('onscroll-show');
+
+        $('.z-navbar2[animate="nav2"]').addClass('wow animate__animated animate__fadeInDown');
+
+    } else {
+        $('.z-navbar1').removeClass('onscroll-hide');
+        $('.z-navbar1').addClass('onscroll-show');
+
+        $('.z-navbar2').removeClass('onscroll-show');
+        $('.z-navbar2').addClass('onscroll-hide');
+        $('.z-navbar1[animate="nav1"]').addClass('wow animate__animated animate__fadeInDown');
+    }
+});
+
+
+
